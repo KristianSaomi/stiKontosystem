1de7eadb6e48d37889e8cecf83293757101d46f6
Kristian <ChristianSaomii@gmail.com>
2021-09-24 15:11:01 +0200

    Vi har parkodat och fixat så hela @Crud request i Controller kan göra request med XML & JSON.

--- src/main/java/com/kontosystem/StiProjekt/repository/UserRepository.java
+++ src/main/java/com/kontosystem/StiProjekt/repository/UserRepository.java
@@ -2,9 +2,14 @@ package com.kontosystem.StiProjekt.repository;
 
 import com.kontosystem.StiProjekt.userEntity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long >  {
 
+    // @Query("SELECT s FROM User s WHERE s.id=?1")
+        // Optional<User> findUserById(Integer id);
  // CRUD förfrågningar ingår i denna Jpa repos, bl.a andra förfrågningar klicka och titta
     User findByName(String name);
 }
--- src/main/java/com/kontosystem/StiProjekt/userController/RestController.java
+++ src/main/java/com/kontosystem/StiProjekt/userController/RestController.java
@@ -3,8 +3,8 @@ package com.kontosystem.StiProjekt.userController;
 
 import com.kontosystem.StiProjekt.userEntity.User;
 import com.kontosystem.StiProjekt.userService.UserService;
-import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -32,27 +32,33 @@ public class RestController {
         return service.saveUsers(users);
     }
 
-    @GetMapping("/users")
+    @GetMapping(path ="/{users}", produces = {MediaType.APPLICATION_XML_VALUE,
+    MediaType.APPLICATION_JSON_VALUE})
     public List<User> findAllUsers() {
         return service.getUsers();
     }
 
-    @GetMapping("/userById/{id}") // Pathvariable, här kan man använda @RequestParam oxå om det önskas
+    @GetMapping(value="/userById/{id}", produces = {MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE}) // Pathvariable, här kan man använda @RequestParam oxå om det önskas
     public User findProductById(@PathVariable long id) {
         return service.getUserById(id);
     }
-
-    @GetMapping("/user/{name}") // denna metod finns inte i JPA repo, skrev in själv. Ta en titt REPO Interface ser du det!!!
+    //Funkar men använd på Unika namn
+    @GetMapping(value="/user/{name}", produces = {MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE}) // denna metod finns inte i JPA repo, skrev in själv. Ta en titt REPO Interface ser du det!!!
     public User findProductByName(@PathVariable String name) {
         return service.getUserByName(name);
     }
 
-    @PutMapping("/update")//Jag får fel meddelande här 404, ska man kanske ha Pathvariable för veta vilken jag ska uppdatera? Eller??
+    @PutMapping(path = "/{update}", consumes ={org.springframework.http.MediaType.APPLICATION_XML_VALUE, org.springframework.http.MediaType.APPLICATION_JSON_VALUE},
+            produces = {org.springframework.http.MediaType.APPLICATION_XML_VALUE, org.springframework.http.MediaType.APPLICATION_JSON_VALUE})
+    //Jag får fel meddelande här 404, ska man kanske ha Pathvariable för veta vilken jag ska uppdatera? Eller??
     public User updateUser(@RequestBody User user) {
         return service.updateUser(user);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping(value="/delete/{id}",
+    produces = {org.springframework.http.MediaType.APPLICATION_XML_VALUE, org.springframework.http.MediaType.APPLICATION_JSON_VALUE})
     public String deleteUser(@PathVariable long id) { return service.deleteUser(id);
 
     }
